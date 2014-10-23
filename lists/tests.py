92e775bf3bb4d848eda66ff82af7b63ebfdc3797
Sandy <slhsith@gmail.com>
2014-10-23 08:52:13 -0700

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -10,24 +10,34 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest() # setup
-        response = home_page(request) # exercise
+        request = HttpRequest()                                    # given
+
+        response = home_page(request)                              # when
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html) # assert
+
+        self.assertEqual(response.content.decode(), expected_html) # then
 
     def test_home_page_can_save_a_POST_request(self):
-        request = HttpRequest() # setup
+        request = HttpRequest()                                    # setup / given
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
-        response = home_page(request) # exercise
+        response = home_page(request)                              # exercise / when
 
-        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.count(), 1)                  # assert / then
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
@@ -36,6 +46,17 @@ class HomePageTest(TestCase):
         request = HttpRequest() # setup test
         home_page(request) # function to be exercised
         self.assertEqual(Item.objects.count(), 0) # assert
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 
 
 class ItemModelTest(TestCase):
